a312a324f588268fc06267e7a58389e51ef0f07e
Martin Larsson <martin.larsson@kognity.com>
2023-03-11 10:46:24 +0100

    Add way to filter out certain diagnotistic messages
    Done since I couldn't get proper settings to propagate down to
    the LSP server, seems ok for now

--- after/plugin/lsp.lua
+++ after/plugin/lsp.lua
@@ -1,16 +1,8 @@
 local lsp = require("lsp-zero")
-
 lsp.preset("recommended")
 
-lsp.ensure_installed({
-	"eslint",
-	"sumneko_lua",
-    "vuels",
-    "jedi_language_server"
-})
-
 -- Fix Undefined global 'vim'
-lsp.configure('sumneko_lua', {
+lsp.configure('lua_ls', {
     settings = {
         Lua = {
             globals = { 'vim' },
@@ -20,6 +12,7 @@ lsp.configure('sumneko_lua', {
         }
     }
 })
+
 
 local cmp = require("cmp")
 local cmp_select = {behavior = cmp.SelectBehavior.Select}
@@ -48,11 +41,6 @@ lsp.set_preferences({
 lsp.on_attach(function(client, bufnr)
   local opts = {buffer = bufnr, remap = false}
 
-  if client.name == "eslint" then
-      vim.cmd.LspStop('eslint')
-      return
-  end
-
   vim.keymap.set("n", "gd", vim.lsp.buf.definition, opts)
   vim.keymap.set("n", "K", vim.lsp.buf.hover, opts)
   vim.keymap.set("n", "<leader>vws", vim.lsp.buf.workspace_symbol, opts)
@@ -64,6 +52,55 @@ lsp.on_attach(function(client, bufnr)
   vim.keymap.set("n", "<leader>vrn", vim.lsp.buf.rename, opts)
   vim.keymap.set("i", "<C-h>", vim.lsp.buf.signature_help, opts)
 end)
+
+function filter(arr, func)
+	-- Filter in place
+	-- https://stackoverflow.com/questions/49709998/how-to-filter-a-lua-array-inplace
+	local new_index = 1
+	local size_orig = #arr
+	for old_index, v in ipairs(arr) do
+		if func(v, old_index) then
+			arr[new_index] = v
+			new_index = new_index + 1
+		end
+	end
+	for i = new_index, size_orig do arr[i] = nil end
+end
+
+
+function filter_diagnostics(diagnostic)
+	-- Only filter out Pyright stuff for now
+	if diagnostic.source ~= "Pyright" then
+		return true
+	end
+
+	-- Allow kwargs to be unused, sometimes you want many functions to take the
+	-- same arguments but you don't use all the arguments in all the functions,
+	-- so kwargs is used to suck up all the extras
+	-- if diagnostic.message == '"kwargs" is not accessed' then
+	-- 	return false
+	-- end
+
+	-- Allow variables starting with an underscore
+	-- if string.match(diagnostic.message, '"_.+" is not accessed') then
+	-- 	return false
+	-- end
+
+    -- Remove diagnostics which start with "Cannot access member"
+	if string.match(diagnostic.message, 'Cannot access member.+') then
+		return false
+	end
+
+	return true
+end
+
+function custom_on_publish_diagnostics(a, params, client_id, c, config)
+	filter(params.diagnostics, filter_diagnostics)
+	vim.lsp.diagnostic.on_publish_diagnostics(a, params, client_id, c, config)
+end
+
+vim.lsp.handlers["textDocument/publishDiagnostics"] = vim.lsp.with(
+	custom_on_publish_diagnostics, {})
 
 lsp.setup()
 
@@ -82,4 +119,3 @@ vim.diagnostic.config({
         prefix = '',
     },
 })
-
