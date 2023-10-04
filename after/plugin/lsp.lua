ff9c150ad973d74f348b8d3b0224ccb5bbfde39c
Martin Larsson <martin.larsson@kognity.com>
2023-10-04 13:05:01 +0200

    Fix setup

--- after/plugin/lsp.lua
+++ after/plugin/lsp.lua
@@ -1,58 +1,58 @@
-local lsp = require("lsp-zero")
-lsp.preset("recommended")
-
--- Fix Undefined global 'vim'
-lsp.configure('lua_ls', {
-    settings = {
-        Lua = {
-            globals = { 'vim' },
-            diagnostics = {
-                globals = { 'vim' }
-            }
-        }
-    }
-})
-
-
-local cmp = require("cmp")
-local cmp_select = { behavior = cmp.SelectBehavior.Select }
-local cmp_mappings = lsp.defaults.cmp_mappings({
-    ["<C-p>"] = cmp.mapping.select_prev_item(cmp_select),
-    ["<C-n>"] = cmp.mapping.select_next_item(cmp_select),
-    ["<CR>"] = cmp.mapping.confirm({ select = true }),
-    ["<C-Space>"] = cmp.mapping.complete(),
-})
-
-
-lsp.setup_nvim_cmp({
-    mapping = cmp_mappings
-})
-
-lsp.set_preferences({
-    suggest_lsp_servers = false,
-    sign_icons = {
-        error = 'E',
-        warn = 'W',
-        hint = 'H',
-        info = 'I'
-    }
-})
-
-lsp.on_attach(function(client, bufnr)
-    local opts = { buffer = bufnr, remap = false }
-
-    vim.keymap.set("n", "gd", vim.lsp.buf.definition, opts)
-    vim.keymap.set("n", "K", vim.lsp.buf.hover, opts)
-    vim.keymap.set("n", "<leader>vws", vim.lsp.buf.workspace_symbol, opts)
-    vim.keymap.set("n", "<leader>vd", vim.diagnostic.open_float, opts)
-    vim.keymap.set("n", "[d", vim.diagnostic.goto_next, opts)
-    vim.keymap.set("n", "]d", vim.diagnostic.goto_prev, opts)
-    vim.keymap.set("n", "<leader>vca", vim.lsp.buf.code_action, opts)
-    vim.keymap.set("n", "<leader>vrr", vim.lsp.buf.references, opts)
-    vim.keymap.set("n", "<leader>vrn", vim.lsp.buf.rename, opts)
-    vim.keymap.set("i", "<C-h>", vim.lsp.buf.signature_help, opts)
-end)
-
+-- local lsp = require("lsp-zero")
+-- lsp.preset("recommended")
+--
+-- -- Fix Undefined global 'vim'
+-- lsp.configure('lua_ls', {
+--     settings = {
+--         Lua = {
+--             globals = { 'vim' },
+--             diagnostics = {
+--                 globals = { 'vim' }
+--             }
+--         }
+--     }
+-- })
+--
+--
+-- local cmp = require("cmp")
+-- local cmp_select = { behavior = cmp.SelectBehavior.Select }
+-- local cmp_mappings = lsp.defaults.cmp_mappings({
+--     ["<C-p>"] = cmp.mapping.select_prev_item(cmp_select),
+--     ["<C-n>"] = cmp.mapping.select_next_item(cmp_select),
+--     ["<CR>"] = cmp.mapping.confirm({ select = true }),
+--     ["<C-Space>"] = cmp.mapping.complete(),
+-- })
+--
+--
+-- lsp.setup_nvim_cmp({
+--     mapping = cmp_mappings
+-- })
+--
+-- lsp.set_preferences({
+--     suggest_lsp_servers = false,
+--     sign_icons = {
+--         error = 'E',
+--         warn = 'W',
+--         hint = 'H',
+--         info = 'I'
+--     }
+-- })
+--
+-- lsp.on_attach(function(client, bufnr)
+--     local opts = { buffer = bufnr, remap = false }
+--
+--     vim.keymap.set("n", "gd", vim.lsp.buf.definition, opts)
+--     vim.keymap.set("n", "K", vim.lsp.buf.hover, opts)
+--     vim.keymap.set("n", "<leader>vws", vim.lsp.buf.workspace_symbol, opts)
+--     vim.keymap.set("n", "<leader>vd", vim.diagnostic.open_float, opts)
+--     vim.keymap.set("n", "[d", vim.diagnostic.goto_next, opts)
+--     vim.keymap.set("n", "]d", vim.diagnostic.goto_prev, opts)
+--     vim.keymap.set("n", "<leader>vca", vim.lsp.buf.code_action, opts)
+--     vim.keymap.set("n", "<leader>vrr", vim.lsp.buf.references, opts)
+--     vim.keymap.set("n", "<leader>vrn", vim.lsp.buf.rename, opts)
+--     vim.keymap.set("i", "<C-h>", vim.lsp.buf.signature_help, opts)
+-- end)
+--
 function filter(arr, func)
     -- Filter in place
     -- https://stackoverflow.com/questions/49709998/how-to-filter-a-lua-array-inplace
@@ -86,7 +86,13 @@ function filter_diagnostics(diagnostic)
     -- end
 
     -- Remove diagnostics which start with "Cannot access member"
-    if string.match(diagnostic.message, 'Cannot access member.+') or string.match(diagnostic.message, 'Argument of type.+') then
+    if (
+        string.match(diagnostic.message, 'Cannot access member.+')
+        or string.match(diagnostic.message, 'Cannot assign member.+') 
+        or  string.match(diagnostic.message, 'Argument of type.+') 
+        or string.match(diagnostic.message, '"kwargs" is not accessed') 
+        or string.match(diagnostic.message, '"args" is not accessed')
+    ) then
         return false
     end
 
@@ -100,9 +106,9 @@ end
 
 vim.lsp.handlers["textDocument/publishDiagnostics"] = vim.lsp.with(
     custom_on_publish_diagnostics, {})
-
-lsp.setup()
-
+--
+-- lsp.setup()
+--
 vim.diagnostic.config({
     virtual_text = true,
     signs = true,
--- lua/martin/lazy.lua
+++ lua/martin/lazy.lua
@@ -1,17 +1,265 @@
+-- BOOTSTRAP the plugin manager `lazy.nvim`
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
-if not vim.loop.fs_stat(lazypath) then
-  vim.fn.system({
-    "git",
-    "clone",
-    "--filter=blob:none",
-    "https://github.com/folke/lazy.nvim.git",
-    "--branch=stable", -- latest stable release
-    lazypath,
-  })
+local lazyIsInstalled = vim.loop.fs_stat(lazypath)
+if not lazyIsInstalled then
+	vim.fn.system({
+		"git",
+		"clone",
+		"--filter=blob:none",
+		"https://github.com/folke/lazy.nvim.git",
+		"--branch=stable",
+		lazypath,
+	})
 end
-vim.opt.rtp:prepend(lazypath)
+vim.opt.runtimepath:prepend(lazypath)
 
 local plugins = {
+    -- TOOLING: COMPLETION, DIAGNOSTICS, FORMATTING
+
+    -- Manager for external tools (LSPs, linters, debuggers, formatters)
+	-- auto-install of those external tools
+	{
+		"WhoIsSethDaniel/mason-tool-installer.nvim",
+		dependencies = {
+			{ "williamboman/mason.nvim", opts = true },
+			{ "williamboman/mason-lspconfig.nvim", opts = true },
+		},
+		opts = {
+			ensure_installed = {
+				"pyright", -- LSP for python
+				"ruff-lsp", -- linter for python (includes flake8, pep8, etc.)
+				"black", -- formatter
+				"isort", -- organize imports
+				"taplo", -- LSP for toml (for pyproject.toml files)
+			},
+		},
+	},
+    --
+    -- Setup the LSPs
+	-- `gd` and `gr` for goto definition / references
+	-- `<leader>c` for code actions (organize imports, etc.)
+    {
+      "neovim/nvim-lspconfig",
+      dependencies = {
+        "hrsh7th/cmp-nvim-lsp",
+        { "antosha417/nvim-lsp-file-operations", config = true },
+      },
+      config = function()
+            -- import lspconfig plugin
+            local lspconfig = require("lspconfig")
+
+            -- import cmp-nvim-lsp plugin
+            local cmp_nvim_lsp = require("cmp_nvim_lsp")
+
+            local keymap = vim.keymap -- for conciseness
+
+            local opts = { noremap = true, silent = true }
+            local on_attach = function(client, bufnr)
+              opts.buffer = bufnr
+
+              -- set keybinds
+              opts.desc = "Show LSP references"
+              keymap.set("n", "gR", "<cmd>Telescope lsp_references<CR>", opts) -- show definition, references
+
+              opts.desc = "Go to declaration"
+              keymap.set("n", "gD", vim.lsp.buf.declaration, opts) -- go to declaration
+
+              opts.desc = "Show LSP definitions"
+              keymap.set("n", "gd", "<cmd>Telescope lsp_definitions<CR>", opts) -- show lsp definitions
+
+              opts.desc = "Show LSP implementations"
+              keymap.set("n", "gi", "<cmd>Telescope lsp_implementations<CR>", opts) -- show lsp implementations
+
+              opts.desc = "Show LSP type definitions"
+              keymap.sst("n", "gt", "<cmd>Telescope lsp_type_definitions<CR>", opts) -- show lsp type definitions
+
+              opts.desc = "See available code actions"
+              keymap.set({ "n", "v" }, "<leader>ca", vim.lsp.buf.code_action, opts) -- see available code actions, in visual mode will apply to selection
+
+              opts.desc = "Smart rename"
+              keymap.set("n", "<leader>vrn", vim.lsp.buf.rename, opts) -- smart rename
+
+              opts.desc = "Show buffer diagnostics"
+              keymap.set("n", "<leader>D", "<cmd>Telescope diagnostics bufnr=0<CR>", opts) -- show  diagnostics for file
+
+              opts.desc = "Show line diagnostics"
+              keymap.set("n", "<leader>d", vim.diagnostic.open_float, opts) -- show diagnostics for line
+
+              opts.desc = "Go to previous diagnostic"
+              keymap.set("n", "[d", vim.diagnostic.goto_prev, opts) -- jump to previous diagnostic in buffer
+
+              opts.desc = "Go to next diagnostic"
+              keymap.set("n", "]d", vim.diagnostic.goto_next, opts) -- jump to next diagnostic in buffer
+
+              opts.desc = "Show documentation for what is under cursor"
+              keymap.set("n", "K", vim.lsp.buf.hover, opts) -- show documentation for what is under cursor
+
+              opts.desc = "Restart LSP"
+              keymap.set("n", "<leader>rs", ":LspRestart<CR>", opts) -- mapping to restart lsp if necessary
+            end
+
+            -- used to enable autocompletion (assign to every lsp server config)
+            local capabilities = cmp_nvim_lsp.default_capabilities()
+
+            -- Change the Diagnostic symbols in the sign column (gutter)
+            -- (not in youtube nvim video)
+            local signs = { Error = " ", Warn = " ", Hint = "󰠠 ", Info = " " }
+            for type, icon in pairs(signs) do
+              local hl = "DiagnosticSign" .. type
+              vim.fn.sign_define(hl, { text = icon, texthl = hl, numhl = "" })
+            end
+			-- setup pyright with completion capabilities
+			lspconfig.pyright.setup({
+				capabilities = lspCapabilities,
+                on_attach = on_attach,
+                settings = {
+                  python = {
+                    analysis = {
+                      autoSearchPaths = true,
+                      diagnosticMode = "workspace",
+                      useLibraryCodeForTypes = true
+                    }
+                  },
+                },
+			})
+
+			-- setup taplo with completion capabilities
+			lspconfig.taplo.setup({
+				capabilities = lspCapabilities,
+                on_attach = on_attach,
+			})
+
+			-- ruff uses an LSP proxy, therefore it needs to be enabled as if it
+			-- were a LSP. In practice, ruff only provides linter-like diagnostics
+			-- and some code actions, and is not a full LSP yet.
+			-- lspconfig.ruff_lsp.setup({
+			-- 	-- organize imports disabled, since we are already using `isort` for that
+			-- 	-- alternative, this can be enabled to make `organize imports`
+			-- 	-- available as code action
+			-- 	settings = {
+			-- 		organizeImports = false,
+			-- 	},
+			-- 	-- disable ruff as hover provider to avoid conflicts with pyright
+			-- 	on_attach = function(client) client.server_capabilities.hoverProvider = false end,
+			-- })
+		end,
+	},
+
+    -- Formatting client: conform.nvim
+	-- - configured to use black & isort in python
+	-- - use the taplo-LSP for formatting in toml
+	-- - Formatting is triggered via `<leader>f`, but also automatically on save
+	{
+		"stevearc/conform.nvim",
+		event = "BufWritePre", -- load the plugin before saving
+		keys = {
+			{
+				"<leader>f",
+				function() require("conform").format({ lsp_fallback = true }) end,
+				desc = "Format",
+			},
+		},
+		opts = {
+			formatters_by_ft = {
+				-- first use isort and then black
+				python = { "isort", "black" },
+			},
+			-- enable format-on-save
+			format_on_save = {
+				-- when no formatter is setup for a filetype, fallback to formatting
+				-- via the LSP. This is relevant e.g. for taplo (toml LSP), where the
+				-- LSP can handle the formatting for us
+				lsp_fallback = true,
+			},
+		},
+	},
+
+    -- Completion via nvim-cmp
+    {
+      "hrsh7th/nvim-cmp",
+      dependencies = {
+        "hrsh7th/cmp-buffer", -- source for text in buffer
+        "hrsh7th/cmp-path", -- source for file system paths
+        "L3MON4D3/LuaSnip", -- snippet engine
+        "saadparwaiz1/cmp_luasnip", -- for autocompletion
+        "rafamadriz/friendly-snippets", -- useful snippets
+        "onsails/lspkind.nvim", -- vs-code like pictograms
+      },
+      config = function()
+        local cmp = require("cmp")
+
+        local luasnip = require("luasnip")
+
+        local lspkind = require("lspkind")
+
+        -- loads vscode style snippets from installed plugins (e.g. friendly-snippets)
+        require("luasnip.loaders.from_vscode").lazy_load()
+
+        cmp.setup({
+          completion = {
+            completeopt = "menu,menuone,preview,noselect",
+          },
+          snippet = { -- configure how nvim-cmp interacts with snippet engine
+            expand = function(args)
+              luasnip.lsp_expand(args.body)
+            end,
+          },
+          mapping = cmp.mapping.preset.insert({
+            ["<C-k>"] = cmp.mapping.select_prev_item(), -- previous suggestion
+            ["<C-j>"] = cmp.mapping.select_next_item(), -- next suggestion
+            ["<C-b>"] = cmp.mapping.scroll_docs(-4),
+            ["<C-f>"] = cmp.mapping.scroll_docs(4),
+            ["<TAB>"] = cmp.mapping.complete(), -- show completion suggestions
+            ["<C-e>"] = cmp.mapping.abort(), -- close completion window
+            ["<CR>"] = cmp.mapping.confirm({ select = false }),
+          }),
+          -- sources for autocompletion
+          sources = cmp.config.sources({
+            { name = "nvim_lsp" },
+            { name = "luasnip" }, -- snippets
+            { name = "buffer" }, -- text within current buffer
+            { name = "path" }, -- file system paths
+          }),
+          -- configure lspkind for vs-code like pictograms in completion menu
+          formatting = {
+            format = lspkind.cmp_format({
+              maxwidth = 50,
+              ellipsis_char = "...",
+            }),
+          },
+        })
+
+
+      end,
+    },
+	-----------------------------------------------------------------------------
+	-- SYNTAX HIGHLIGHTING    -- treesitter for syntax highlighting
+	-- - auto-installs the parser for python
+	{
+		"nvim-treesitter/nvim-treesitter",
+		-- automatically update the parsers with every new release of treesitter
+		build = ":TSUpdate",
+	},
+
+    -- Long classes/functions context
+    -- 'nvim-treesitter/nvim-treesitter-context',
+
+	-----------------------------------------------------------------------------
+    -- OTHER
+
+    -- f-strings
+	-- - auto-convert strings to f-strings when typing `{}` in a string
+	-- - also auto-converts f-strings back to regular strings when removing `{}`
+	{
+		"chrisgrieser/nvim-puppeteer",
+		dependencies = "nvim-treesitter/nvim-treesitter",
+	},
+
+    -- better indentation behavior
+	-- by default, vim has some weird indentation behavior in some edge cases,
+	-- which this plugin fixes
+	{ "Vimjas/vim-python-pep8-indent" },
+
     -- Telescope, used for project, git and grep search
      {
         'nvim-telescope/telescope.nvim', version = '0.1.0',
@@ -35,12 +283,6 @@ local plugins = {
         version = 'nightly' -- optional, updated every week. (see issue #1193)
     },
 
-    -- Gives syntax coloring, might need to add languages
-    {'nvim-treesitter/nvim-treesitter', build = ':TSUpdate' },
-
-    -- Long classes/functions context
-    'nvim-treesitter/nvim-treesitter-context',
-
     -- Allows me to bookmark and easily move between different
     -- files. Might need more keybinds if 4 windows turns out
     -- to be too little
@@ -56,29 +298,6 @@ local plugins = {
 
     -- Git integration
     {'tpope/vim-fugitive', dependencies = { 'tpope/vim-rhubarb' }},
-
-    -- LSP
-     {
-        'VonHeikemen/lsp-zero.nvim',
-        dependencies = {
-            -- LSP Support
-            'neovim/nvim-lspconfig',
-            'williamboman/mason.nvim',
-            'williamboman/mason-lspconfig.nvim',
-
-            -- Autocompletion
-            'hrsh7th/nvim-cmp',
-            'hrsh7th/cmp-buffer',
-            'hrsh7th/cmp-path',
-            'saadparwaiz1/cmp_luasnip',
-            'hrsh7th/cmp-nvim-lsp',
-            'hrsh7th/cmp-nvim-lua',
-
-            -- Snippets
-            'L3MON4D3/LuaSnip',
-            'rafamadriz/friendly-snippets',
-        }
-    },
 
     -- Copilot
      'zbirenbaum/copilot.lua',
@@ -108,11 +327,6 @@ local plugins = {
         end
     },
 
-    -- Formatter
-    "williamboman/mason.nvim",
-    "jose-elias-alvarez/null-ls.nvim",
-    "jay-babu/mason-null-ls.nvim",
-
     -- Testing
      "vim-test/vim-test",
 
@@ -129,9 +343,7 @@ local plugins = {
         'nvim-lualine/lualine.nvim',
         dependencies = { 'kyazdani42/nvim-web-devicons', lazy = true }
     },
-
-
-
 }
 
 require("lazy").setup(plugins)
+vim.g.mapleader = " "
