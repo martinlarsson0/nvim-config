beeddc571446cf159dab59fa40b4e759a69fe47a
Martin Larsson <martin.larsson@kognity.com>
2023-10-04 18:36:39 +0200

    Update fzf config

--- lua/martin/lazy.lua
+++ lua/martin/lazy.lua
@@ -335,9 +335,7 @@ local plugins = {
      "nvim-telescope/telescope-file-browser.nvim",
 
     -- Faster telescope sorting
-    { 'nvim-telescope/telescope-fzf-native.nvim',
-        build = 'cmake -S. -Bbuild -DCMAKE_BUILD_TYPE=Release && cmake --build build --config Release && cmake --install build --prefix build'
-    },
+    { 'nvim-telescope/telescope-fzf-native.nvim', build = 'cmake -S. -Bbuild -DCMAKE_BUILD_TYPE=Release && cmake --build build --config Release && cmake --install build --prefix build' },
 
     {
         'nvim-lualine/lualine.nvim',
