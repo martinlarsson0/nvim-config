ea9ae9a52803537f9c5f4a167099f6f49439ad09
Martin Larsson <martin.larsson@kognity.com>
2024-04-20 18:28:03 +0200

    Do not lazy lode vim-test

--- lua/martin/lazy/vim-test.lua
+++ lua/martin/lazy/vim-test.lua
@@ -1,5 +1,6 @@
 return {
 	"vim-test/vim-test",
+	lazy = false,
 	config = function()
 		vim.g["test#python#djangotest#options"] = {
 			all = 'DJANGO_SETTINGS_MODULE="kog.config.settings.testing" --keepdb',
